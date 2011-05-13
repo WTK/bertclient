97fccffe14987af0f13071e43d7cf0230a387ea2
Marcin Wtorkowski <marcin.wtorkowski@gmail.com>
2011-05-13 11:56:03 +0200

    -

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,3 @@ require 'bert'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 load 'bertclient_new.rb'
-
-def to_bytes(str)
-  bytes = []
-  str.each_byte {|b| bytes << b }
-  bytes
-end
